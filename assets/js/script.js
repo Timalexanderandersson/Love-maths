d09b4117687201eb3e9cbfc6ae84f7e336a0f916
Tim Andersson <timalexanderandersson@hotmail.com>
2024-03-13 13:18:30 +0000

    added the struckture for javascript game struckture.

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtrackQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+
