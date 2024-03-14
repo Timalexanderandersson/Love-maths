c5ad036bdc5f7564ab1f6fdd28b956ca5b474942
Tim Andersson <timalexanderandersson@hotmail.com>
2024-03-14 09:23:31 +0000

    add the addition part to the site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,39 @@
 // wait for the DOM to load before running the game.
 //get the button element and add eventlisteners
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-    for (let button of buttons){
-        button.addEventListener('click', function() {
-            if (this.getAttribute("data-type") === "submit"){
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        })
+        });
     }
-})
+
+    runGame("addition");
+
+});
 /** the main Game "loop" called when script is loaded
  * after the user answer the processed.
  */
 
-function runGame(){
-    //Created 2 random numbers between 1-25.
-    let num1 = Math.floor(Math.random() * 25) +1 ;
-    let num2 = Math.floor(Math.random() * 25) +1 ;
+function runGame(gameType) {
+
+    // Create two random numbers 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -35,8 +48,11 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 function displaySubtrackQuestion() {
 
@@ -44,4 +60,3 @@ function displaySubtrackQuestion() {
 function displayMultiplyQuestion() {
 
 }
-
